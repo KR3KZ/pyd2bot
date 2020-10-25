5bc0198f7cd762e82106a7d9d3ac9955b170559b
khalid.majdoub <majdoub.khalid@gmail.com>
2020-10-25 13:17:31 +0100

    Finished reconding the highlighter

--- testGetMapCells.sikuli/testGetMapCells.py
+++ testGetMapCells.sikuli/testGetMapCells.py
@@ -1,7 +1,5 @@
 from java.awt import Robot
-from java.awt import Color
-from java.awt import Rectangle
-import java.awt.Color as Color
+from java.awt import Color, Rectangle, AlphaComposite, RenderingHints, BasicStroke
 import java.awt.Toolkit
 import java.awt.image.BufferedImage
 import java.io.File
@@ -10,11 +8,74 @@ import javax.imageio.ImageIO as ImageIO
 import time
 import logging
 import json
+from javax.swing import JFrame, JPanel, ImageIcon, JLabel
+from java.awt.event import MouseListener
+import traceback
+
 
 SRC_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
 COMBAT_R = Region(335, 29, 1253, 885)
 NBR_H_CELL = 14.5
 NBR_V_CELL = 20.5
+
+
+class ScreenHighlighter(JFrame):
+    opened = set()
+
+    def __init__(self):
+        super(ScreenHighlighter, self).__init__()
+        self.target_color = Color(1, 0, 0, 0.7)
+        self.setAlwaysOnTop(True)
+        self.setUndecorated(True)
+        self.setBackground(Color(0, 0, 0, 0.0))
+        self.roi = set()
+        self.opened.add(self)
+        self.setVisible(False)
+        self.getRootPane().putClientProperty("Window.shadow", False)
+            
+    def closeAfter(self, secs):
+        try:
+            sleep(secs)
+        except InterruptedError as e:
+            self.close()
+            traceback.print_stack()
+        self.close()
+
+    def drawBorder(self, g):
+        stroke = BasicStroke(3)
+        g.setColor(self.target_color)
+        g.setStroke(stroke)
+        w = stroke.getLineWidth()
+        g.drawRect(int(w / 2), int(w / 2), int(self.getWidth() - w), int(self.getHeight() - w))
+
+    def drawRoi(self, g):
+        print('yep')
+        g.setColor(self.target_color)
+        for loc in self.roi:
+            g.drawLine(loc.x, loc.y, loc.x, loc.y)
+
+    def paint(self, g):
+        super(ScreenHighlighter, self).paint(g)
+        if not self.roi:
+            self.drawBorder(g)
+        else:
+            self.drawRoi(g)
+
+    def close(self):
+        self.setVisible(False)
+        self.opened.remove(self)
+        self.dispose()
+
+    def highlight(self, r, secs=None, roi=None, color=None):
+        self.setLocation(r.x, r.y)
+        self.setSize(r.w, r.h)
+        if roi:
+            self.roi = roi
+        if color:
+            self.targetColor = color
+        self.setVisible(True)
+        if secs:
+            self.closeAfter(secs)
 
 
 class CellType:
@@ -23,7 +84,6 @@ class CellType:
     BOT = 2
     FREE = 3
     REACHABLE = 4
-
 
 class Cell(Location):
 
@@ -56,6 +116,7 @@ class Cell(Location):
 
 
 class Grid(list):
+    
     def __init__(self, region, nbr_vcell, nbr_hcell):
         list.__init__(self)
         self.nbr_vcell = nbr_vcell
--- testPixelPainting.sikuli/testPixelPainting.py
+++ testPixelPainting.sikuli/testPixelPainting.py
@@ -1,146 +1,77 @@
-from java.awt import Color, Rectangle
-from java.awt.event import *
-from java.awt.image import BufferedImage
-from java.swing import JFrame, JPanel, ImageIcon, JLabel
+from java.awt import Color, Rectangle, AlphaComposite, RenderingHints, BasicStroke
+from javax.swing import JFrame, JPanel, ImageIcon, JLabel
+from java.awt.event import MouseListener
+import traceback
+from time import sleep
 
 
 class ScreenHighlighter(JFrame):
-    _opened = set()
-    bi = None
-    _lastTarget = None
-    _anim = None
-    TARGET_SIZE = 80
-    MARGIN = 20
+    opened = set()
 
-    def __init__(self, screen):
-        self._native_transparent = True
-        self.double_buffered = True
-        self._overlayColor = Color(0, 0, 0, 0, 0)
-        self._transparentColor = Color(0, 0, 0, 0.0)
-        self._targetColor = Color(1, 0, 0, 0.7)
-        self._opened.add(self)
-        self.srcx, self.srcy, self.destx, self.desty = None
-        self._lastTarget = None
-        self._native_transparent = False
-        self.double_buffered = False
-        self._StrokeCross = BasicStroke(1, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 1, lambda x:2, 0)
-        self._StrokeBorder = BasicStroke(3)
-        self._screen = screen
-        self.setVisible(False)
+    def __init__(self):
+        super(ScreenHighlighter, self).__init__()
+        self.target_color = Color(1, 0, 0, 0.7)
         self.setAlwaysOnTop(True)
-        self.setOpaque(False)
         self.setUndecorated(True)
-        if self._native_transparent:
-            self.setBackground(self._transparentColor)
+        self.setBackground(Color(0, 0, 0, 0.0))
+        self.roi = set()
+        self.opened.add(self)
+        self.setVisible(False)
         self.getRootPane().putClientProperty("Window.shadow", False)
-        self.getContentPane().setDoubleBuffered(True)
-        self.addMouseListener(self)
-
-    def mouseClicked(self, e):
-        self.dispose()
-
+            
     def closeAfter(self, secs):
         try:
-            time.sleep(secs * 1000)
+            sleep(secs)
         except InterruptedError as e:
             self.close()
             traceback.print_stack()
         self.close()
 
-    def captureScreen(self, x, y, w, h):
-        img = self.screen.capture(x, y, w, h)
-        self._screen = img.getImage()
-        scaleFactor = .6
-        op = RescaleOp(scaleFactor, 0, None)
+    def drawBorder(self, g):
+        stroke = BasicStroke(3)
+        g.setColor(self.target_color)
+        g.setStroke(stroke)
+        w = stroke.getLineWidth()
+        g.drawRect(int(w / 2), int(w / 2), int(self.getWidth() - w), int(self.getHeight() - w))
 
-    def drawCircle(self, x, y, radius, g):
-        g.drawOval(x - radius, y - radius, radius * 2, radius * 2)
-
-    def drawTarget(self, g2d):
-        cx = (self.srcx + self.destx) / 2
-        cy = (self.srcy + self.desty) / 2
-
-        if self._anim.running():
-            g2d.setColor(self._targetColor)
-            g2d.setStroke(self._StrokeBorder)
-            size = self._anim.step()
-            size2 = 0 if size == 0 else size - 5
-            self.drawCircle(cx, cy, size, g2d)
-            self.drawCircle(cx, cy, size2, g2d)
-            self.repaint()
-
-    def drawBorder(self, g2d):
-        g2d.setColor(self._targetColor)
-        g2d.setStroke(self._StrokeBorder)
-        w = self._StrokeBorder.getLineWidth()
-        g2d.drawRect(w / 2, w / 2, self.getWidth() - w, self.getHeight() - w)
+    def drawRoi(self, g):
+        print('yep')
+        g.setColor(self.target_color)
+        for loc in self.roi:
+            g.drawLine(loc.x, loc.y, loc.x, loc.y)
 
     def paint(self, g):
-        super.paint(g)
-        if not self.bi or self.bi.getWidth(self) != self.getWidth() or self.bi.getHeight(self) != self.getHeight():
-            self.bi = BufferedImage(self.getWidth(), self.getHeight(), BufferedImage.TYPE__RGB)
-
-        bfG2 = self.bi.createGraphics()
-        g2d = None
-
-        if self.double_buffered:
-            g2d = bfG2
-
+        super(ScreenHighlighter, self).paint(g)
+        if not self.roi:
+            self.drawBorder(g)
         else:
-            g2d = g
-            g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.CLEAR, 0.0))
-            g2d.fillRect(0, 0, self.getWidth(), self.getHeight())
-            g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0))
-            g2d.setRenderingH(RenderingHs.KEY_ANTIALIASING, RenderingHs.VALUE_ANTIALIAS_ON)
-
-        self.drawTarget(g2d)
-
-        if self.double_buffered:
-            g.drawImage(self.bi, 0, 0, self)
-            if not self.isVisible():
-                self.setVisible(True)
-
-        elif self.isVisible():
-            self.setVisible(False)
+            self.drawRoi(g)
 
     def close(self):
         self.setVisible(False)
-        for it in self._opened:
-            it.remove(self)
+        self.opened.remove(self)
         self.dispose()
 
-    def closeAll(self):
-        log.info(3, "close all ScreenHighlighter")
-        for s in self._opened:
-            if s.self.isVisible():
-                s.self.setVisible(False)
-                s.dispose()
-        self._opened.clear()
-
-    def highlight(self, r_, secs=None):
-        r = None
-        if self._native_transparent:
-            r = r_
-        else:
-            r = Region(r_)
-            r.setROI(Rectangle(r_.x - 3, r_.y - 3, r_.w + 6, r_.h + 6))
-            self.captureScreen(r.x, r.y, r.w, r.h)
+    def highlight(self, r, secs=None, roi=None, color=None):
         self.setLocation(r.x, r.y)
         self.setSize(r.w, r.h)
-        self.setBackground(self._transparentColor)
+        if roi:
+            self.roi = roi
+        if color:
+            self.targetColor = color
         self.setVisible(True)
-        self.toFront()
         if secs:
             self.closeAfter(secs)
 
-    def showWindow(self, x, y, w, h, secs):
-        self.setLocation(x, y)
-        self.setSize(w, h)
-        self.setVisible(True)
-        self.toFront()
-        self.closeAfter(secs)
 
+r = Region(696, 395, 460, 255)
+overlay = ScreenHighlighter()
+overlay.setSize(r.w, r.h)
+overlay.setLocation(r.x, r.y)
+overlay.setVisible(True)
+for x in range(100):
+    overlay.roi.add(Location(x, x))
+    overlay.repaint() 
+overlay.close()
+sleep(2)
 
-scr = r.getScreen()
-_overlay = ScreenHighlighter(scr)
-_overlay.highlight(r)
