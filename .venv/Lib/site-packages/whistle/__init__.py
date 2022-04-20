0543928dac515fe78b081b00e0d74cc1e7d99aee
unknown <majdoub.khalid@gmail.com>
2022-04-20 09:40:33 +0200

    delete venv and vscode folders

--- .venv/Lib/site-packages/whistle/__init__.py
+++
@@ -1,9 +0,0 @@
-from whistle._version import __version__
-from whistle.dispatcher import EventDispatcher
-from whistle.event import Event
-
-__all__ = [
-    'Event',
-    'EventDispatcher',
-    '__version__',
-]
